ad6572239a25d4b85314bf7f61e0daa774938c95
huyuyu1226 <hyupuka@icloud.com>
2021-04-11 19:28:38 +0900

    コミットメッセージ

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <input type="text" v-model="keyword" />
+    <button @click="add">住所自動入力</button>
+    <p>Address：{{ show }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      keyword: "",
+      show: "",
+      apiKey: "OXeaE3yV5GOSKP8k3CJt6VuSF2ecvRA7IP34FZV",
+    };
+  },
+  methods: {
+    add() {
+      axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.keyword}?apiKey=OXeaE3yV5GOSKP8k3CJt6VuSF2ecvRA7IP34FZV`).then((res) => {
+        this.show = res.data[0].allAddress;
+      });
+    },
+  },
+};
+</script>
